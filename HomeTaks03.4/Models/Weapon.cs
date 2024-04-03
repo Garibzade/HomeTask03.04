01ab5534fed83545a82c053b533ab1b437c5c687
Garibzade <161576671+Garibzade@users.noreply.github.com>
2024-04-03 06:25:23 +0400

    Added Project

---
+++ HomeTaks03.4/Models/Weapon.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeTaks03._4
+{
+    internal class Weapon
+    {
+        int _bulletCapacity;
+        int _bulletCaunt;
+        int _finishTime;
+        private bool _single = true;
+
+        public int BulletCapacity {  get { return _bulletCapacity; } set {  _bulletCapacity = value; } }
+        public int BulletCaunt { get { return _bulletCaunt;} set { _bulletCaunt = value;} }
+        public int FinishTime {  get { return _finishTime; } set {  _finishTime = value; } }
+        public bool Single { get { return _single; } set { _single = value; } } 
+
+        public Weapon(int bulletCapacity,int bulletcaunt,int finishtime,bool single) 
+        {
+            _bulletCapacity = bulletCapacity;
+            _bulletCaunt = bulletcaunt;
+            _finishTime = finishtime;
+            _single = single;
+
+        }
+        public void Shot()
+        {
+            if (BulletCaunt > 0)
+            {
+                Console.WriteLine("PARRTTT");
+                BulletCaunt--;
+            }
+            else { Console.WriteLine("Silahda gulle yoxdur zehmet olmasa reloud ele.Dusmenler gelir"); }
+        } 
+        public void Fire()
+        {
+            if (BulletCaunt > 0) 
+            {
+                Console.WriteLine("Butun gulleri bosaltdin");
+                Console.WriteLine("Gulleler "+BulletCaunt*FinishTime+"saniyeyey bitdi");
+                BulletCaunt = 0;
+            }
+            else
+            {
+                Console.WriteLine( "Silahda gulle yoxdur zehmet olmasa reloud ele. Dusmenler gelir");
+            }
+        }
+
+        public void GetRemainBulletCount()
+        {
+            Console.WriteLine("Daragin tam dolmasi ucun lazim olan gulle sayi" + " "+ (BulletCapacity-BulletCaunt));
+        }
+
+        public void Reloud()
+        {
+            BulletCaunt=BulletCapacity;
+            Console.WriteLine("Reloud olundu");
+        }
+        public void ChangeFireMode()
+        {
+            if (Single)
+            {
+                Single= false;
+                Console.WriteLine("Silah otamatike cevrildi");
+            }
+            else if (!Single)
+            {
+                Single = true;
+                Console.WriteLine("Silah tekliye cevrildi");
+            }
+
+        }
+
+
+
+    }
+}
---
+++ HomeTaks03.4/Program.cs
@@ -0,0 +1,88 @@
+﻿namespace HomeTaks03._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Weapon weapon=new Weapon(bulletCapacity:30,bulletcaunt:30,finishtime:2,true);
+            while (true) 
+            { 
+            Console.WriteLine("0 - İnformasiya almaq üçün\r\n1 - Shoot metodu üçün\r\n2 - Fire metodu üçün\r\n3 - GetRemainBulletCount metodu üçün\r\n4 - Reload metodu üçün\r\n5 - ChangeFireMode metodu üçün\r\n6 - Proqramdan dayandırmaq üçün\r\nqısayoldur.\n7 - Redaktə et :");
+            string choose=Console.ReadLine();
+                switch (choose)
+                {
+                    case "0":
+                        Console.WriteLine($"Daragin tutumu:{weapon.BulletCapacity}\nGulle sayi:{weapon.BulletCaunt}\nGullerin bitme vaxti:{weapon.FinishTime}\nSilah teklidedir:{weapon.Single} \n");
+                       
+                       
+                        break;
+                        case "1":
+                        weapon.Shot();
+                        break;
+                        case "2":
+                        weapon.Fire();
+                        break;
+                        case "3":
+                        weapon.GetRemainBulletCount();
+                        break;
+                        case "4":
+                        weapon.Reloud();
+                       break;
+                        case "5":
+                        weapon.ChangeFireMode();
+                        break;
+                        case "6":
+                       
+                    case "7":
+                        Console.WriteLine("Y\nS\nD");
+                        string minichoose =Console.ReadLine();
+                        switch(minichoose)
+                        {
+                            case "y":
+                                int value=int.Parse(Console.ReadLine());
+                                Console.WriteLine("capasite sayi="+(value+weapon.BulletCapacity));
+                                break;
+                            //case "Y":
+                            //    int num = int.Parse(Console.ReadLine());
+                            //    Console.WriteLine("deyisdikden soraki sayi:"+(num + weapon.BulletCapacity));  //muellim bunlarida verdimki isdifadeci boyuk kicik yazanda yene islesin
+                            //    break;
+                            case "s":
+                                int sum=int.Parse(Console.ReadLine());
+                                Console.WriteLine( "gule sayi ="+(sum+weapon.BulletCapacity));
+                                break;
+                            //case "S":
+                            //    int sum2=int.Parse(Console.ReadLine());
+                            //    Console.WriteLine("gule sayi ="+(sum2 + weapon.BulletCapacity));
+                            //    break;
+                            case "d":
+                                int time=int.Parse(Console.ReadLine());
+                                Console.WriteLine("reload olma vatdi:"+(time+weapon.FinishTime));
+                                break;
+                            //case "D":
+                            //    int time2=int.Parse(Console.ReadLine());
+                            //    Console.WriteLine("sifirlanma saniyesi ="+(time2+weapon.FinishTime));
+                            //    break;
+                            default: Console.WriteLine("Menyuda olan ededi secin zehmet olmasa!!");
+                                break;
+
+
+                        }
+                        break;
+                        return;
+
+                    default: Console.WriteLine("Menyuda olan ededi secin zehmet olmasa!!!");
+                        break;
+                }
+                Console.ReadKey();
+                Console.Clear();
+
+
+
+
+
+
+
+            }
+        }
+    }
+}
